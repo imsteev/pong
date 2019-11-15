b38b4febf2df74e8fcab82cbb444efd377ecf789
Stephen Chung <stephenchung@Stephens-MacBook-Pro.local>
2019-11-15 11:29:19 -0800

    add test to make sure everyone plays each other (even only)

--- round_robin.py
+++ round_robin.py
@@ -10,15 +10,15 @@ class Circle:
     def __init__(self, L):
         self.circle = deque(L or [])
 
+    def __len__(self):
+        return len(self.circle)
+
     def __getitem__(self, i):
         """
         Return i-th element of the circle, where 0-th element is at 12 o'clock
         """
         # TODO: safety checks
         return self.circle[i]
-
-    def __len__(self):
-        return len(self.circle)
 
     def rotate(self, clockwise=True):
         if not self.circle:
@@ -37,6 +37,15 @@ class Circle:
         return self.circle.popleft() if self.circle else None
 
 
+def format_matchup(p1, p2, dummy_player):
+    if p1 == dummy_player:
+        return "BYE", p2
+    elif p2 == dummy_player:
+        return "BYE", p1
+    else:
+        return min(p1, p2), max(p1, p2)
+
+
 def round_robin(n):
     """
     https://en.wikipedia.org/wiki/Round-robin_tournament
@@ -44,30 +53,37 @@ def round_robin(n):
     Create a round-robin schedule for n players.
     If n is odd, there will be matchups with BYEs for each round.
     """
-    bye_num = None
+    if n < 2:
+        print("No round robin exists for schedule with less than two players")
+        return
+
+    # normalize to even amount of players
+    dummy_player = None
     if n % 2 == 1:
         n += 1
-        bye_num = n
+        dummy_player = n
 
-    # Construct rotatable circle with everyone but first player
+    # construct rotatable circle with everyone but first player
     circle = Circle(list(range(2, n+1)))
 
     for _ in range(n-1):
-        round = []  # round
+        round = []
 
         # fix the first player in the circle
         circle.insert_beginning(1)
 
-        for i in range(n // 2):
-            p1, p2 = circle[i], circle[n-1-i]
-            if p1 == bye_num:
-                p1 = -1
-            if p2 == bye_num:
-                p2 = -1
-            round.append((min(p1, p2), max(p1, p2)))
+        for i in range(n//2):
+            p1, p2 = circle[i], circle[n-1-i]  # play the person "opposite" of you in the circle
+            matchup = format_matchup(p1, p2, dummy_player)
+            round.append(matchup)
 
         yield round
 
         # rotate everyone else but the first player
         circle.remove_beginning()
         circle.rotate()
+
+
+if __name__ == "__main__":
+    for i, round in enumerate(round_robin(10), 1):
+        print("ROUND {0}: {1}".format(i, round))
--- test_round_robin.py
+++ test_round_robin.py
@@ -23,3 +23,27 @@ def test_rotate_counter_clockwise():
     assert circle[0] == 2
     assert circle[1] == 3
     assert circle[2] == 1
+
+
+def test_round_robin_invalid_players():
+    for i in range(2):
+        matchups = list(round_robin.round_robin(i))
+        assert matchups == []
+
+
+def test_even_round_robin_everyone_plays_each_other():
+    num_players = 10
+    counts = {}
+    for round in round_robin.round_robin(num_players):
+        for p1, p2 in round:
+            if p1 not in counts:
+                counts[p1] = set()
+            if p2 not in counts:
+                counts[p2] = set()
+            counts[p1].add(p2)
+            counts[p2].add(p1)
+
+    assert len(counts) == num_players
+    for p in counts:
+        assert len(counts[p]) == num_players - 1
+        assert p not in counts[p]
