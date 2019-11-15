6d8192a5dca3b6a42e4c01a9a8bd22b06b4c41f4
Stephen Chung <stephenchung@Stephens-MacBook-Pro.local>
2019-11-15 13:37:56 -0800

    use dataclass in script

--- round_robin.py
+++ round_robin.py
@@ -74,19 +74,28 @@ def round_robin(n):
 
 
 if __name__ == "__main__":
-    NUM_PLAYERS = 7  # adjust this accordingly
-    DUMMY_PLAYER = {"rank": '-', 'name': "BYE"}
+    from dataclasses import dataclass
+    @dataclass
+    class Player:
+        rank: int = "-"
+        name: str = "BYE"
 
-    players = [{"rank": 1, "name": "Michael Gary Scott"},
-               {"rank": 2, "name": "Jim Halpert"},
-               {"rank": 3, "name": "Pam Beasley"},
-               {"rank": 4, "name": "Kevin Malone"},
-               {"rank": 5, "name": "Phyllis Vance"},
-               {"rank": 6, "name": "Dwight Schrute"},
-               {"rank": 7, "name": "Robert California"},
-               {"rank": 8, "name": "Kelly Kapoor"}]
+    NUM_PLAYERS = 6  # adjust this accordingly
+    DUMMY_PLAYER = Player()
 
-    players.sort(key=lambda info: info['rank'])
+    players = [Player(rank=1, name='Michael Gary Scott'),
+               Player(rank=2, name='Dwight K. Schrute'),
+               Player(rank=3, name='Jim Halpert'),
+               Player(rank=4, name='Pam Beasley'),
+               Player(rank=5, name='Oscar Martinez'),
+               Player(rank=6, name='Phyllis Vance, Vance Refrigeration'),
+               Player(rank=7, name='Angela Martin'),
+               Player(rank=8, name='Stanley Hudson'),
+               Player(rank=9, name='Kelly Kapoor'),
+               Player(rank=10, name='Ryan Howard'),
+               Player(rank=11, name='Robert California')]
+
+    players.sort(key=lambda p: p.rank)
 
     for i, round in enumerate(round_robin(NUM_PLAYERS), 1):
         print("ROUND {}".format(i))
@@ -100,5 +109,5 @@ if __name__ == "__main__":
                 player2 = players[p2-1]
             else:
                 player2 = DUMMY_PLAYER
-            print("{0} ({1}) v. {2} ({3})".format(player1['name'], player1['rank'], player2['name'], player2['rank']))
+            print("{0} ({1}) v. {2} ({3})".format(player1.name, player1.rank, player2.name, player2.rank))
         print()
