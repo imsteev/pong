1fa6a16da7dc09f676e1ef90f97d243e50ed160d
Stephen Chung <spchung95@gmail.com>
2019-11-15 21:02:58 -0800

    update server

--- app.py
+++ app.py
@@ -17,29 +17,25 @@ app = Flask(__name__)
 PLAYER_POOL = None
 ROUND_ROBIN_GENERATOR = None
 CURRENT_ROUND = 0
+CURRENT_MATCHUPS = None
 
 
 @app.route('/', methods=['GET'])
 def index():
+    global CURRENT_MATCHUPS
     global CURRENT_ROUND
     global PLAYER_POOL
     global ROUND_ROBIN_GENERATOR
-    matchups_str = "No players registered"
-    if ROUND_ROBIN_GENERATOR is not None:
-        try:
-            matchups = get_matchups(next(ROUND_ROBIN_GENERATOR), PLAYER_POOL)
-            matchups_str = '\n'.join(["{0} ({1}) v. {2} ({3})".format(
-                p1.name, p1_seed, p2.name, p2_seed) for ((p1, p1_seed), (p2, p2_seed)) in matchups])
-            CURRENT_ROUND += 1
-        except StopIteration:
-            CURRENT_ROUND = 0
-            matchups_str = "Tournament finished!"
 
-    if CURRENT_ROUND == 0:
-        return matchups_str
-    else:
+    if CURRENT_MATCHUPS:
+        matchups_str = '\n'.join(
+            ["{0} ({1}) v. {2} ({3})".format(p1.name, p1_seed, p2.name, p2_seed)
+             for ((p1, p1_seed), (p2, p2_seed)) in CURRENT_MATCHUPS])
         return f"""Round {CURRENT_ROUND}
         {matchups_str}"""
+    else:
+        CURRENT_ROUND = 0
+        return "No matchups. Reset the tournament or start a new one!"
 
 
 @app.route('/tournament', methods=['GET', 'POST'])
@@ -50,6 +46,20 @@ def tournament():
         PLAYER_POOL = [Player(**p) for p in request.get_json()]
         ROUND_ROBIN_GENERATOR = round_robin(len(PLAYER_POOL))
     return jsonify(PLAYER_POOL)
+
+
+@app.route('/next_round', methods=['GET'])
+def next_round():
+    global CURRENT_MATCHUPS
+    global CURRENT_ROUND
+    global ROUND_ROBIN_GENERATOR
+    if ROUND_ROBIN_GENERATOR is not None:
+        try:
+            CURRENT_MATCHUPS = get_matchups(next(ROUND_ROBIN_GENERATOR), PLAYER_POOL)
+            CURRENT_ROUND += 1
+        except StopIteration:
+            CURRENT_MATCHUPS = None
+    return redirect(url_for('index'))
 
 
 @app.route('/reset', methods=['GET'])
