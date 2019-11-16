5dc23e3cb81a2ea365b30e2995b00853b0690c9b
Stephen Chung <spchung95@gmail.com>
2019-11-16 14:39:58 -0800

    silly index.html

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ Simple Flask server that can host a single round-robin tournament (in-memory, fo
 import json
 import os
 
-from flask import Flask, jsonify, redirect, request, url_for
+from flask import Flask, jsonify, redirect, request, send_from_directory, url_for
 
 from models.player import Player
 from round_robin import construct_matchups, format_matchup, round_robin
@@ -12,10 +12,10 @@ from round_robin import construct_matchups, format_matchup, round_robin
 DEFAULT_HOST = "localhost"
 DEFAULT_PORT = 8888
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 CACHE = dict()
-CACHE['pool'] = None  # pool of Players
+CACHE['players'] = None
 CACHE['round_robin'] = None
 CACHE['current_round'] = None
 CACHE['current_round_num'] = 0
@@ -23,23 +23,24 @@ CACHE['current_round_num'] = 0
 
 @app.route('/', methods=['GET'])
 def index():
-    global CACHE
-    if CACHE['current_round']:
-        matchups_str = '\n'.join([format_matchup(p1, p1_seed, p2, p2_seed)
-                                  for ((p1, p1_seed), (p2, p2_seed)) in CACHE['current_round']])
-        msg = "Round {}\n{}".format(CACHE['current_round_num'], matchups_str)
-    else:
-        msg = "No matchups. Reset the tournament or start a new one!"
-    return msg
+    return app.send_static_file('index.html')
 
 
 @app.route('/tournament', methods=['GET', 'POST'])
 def tournament():
     global CACHE
     if CACHE['round_robin'] is None:
-        CACHE['pool'] = [Player(**p) for p in request.get_json()]
-        CACHE['round_robin'] = round_robin(len(CACHE['pool']))
-    return f"{len(CACHE['pool'])} player(s) registered. Navigate to /next_round to start!"
+        CACHE['players'] = [Player(**p) for p in request.get_json()]
+        CACHE['round_robin'] = round_robin(len(CACHE['players']))
+    return f"{len(CACHE['players'])} player(s) registered. Navigate to /next_round to start!"
+
+
+@app.route('/players', methods=['GET', 'POST'])
+def players():
+    global CACHE
+    if request.method == 'GET':
+        return jsonify(CACHE['players'])
+    return url_for('index')
 
 
 @app.route('/next_round', methods=['GET'])
@@ -48,7 +49,7 @@ def next_round():
     if CACHE['round_robin']:
         try:
             next_round = next(CACHE['round_robin'])
-            CACHE['current_round'] = construct_matchups(next_round, CACHE['pool'])
+            CACHE['current_round'] = construct_matchups(next_round, CACHE['players'])
             CACHE['current_round_num'] += 1
         except StopIteration:
             return redirect(url_for('reset'))
