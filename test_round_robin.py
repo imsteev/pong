85ae0ba481ebe6fb67a702f0a10f56b69892b7d2
Stephen Chung <spchung95@gmail.com>
2019-11-15 21:59:19 -0800

    make tests clearer

--- test_round_robin.py
+++ test_round_robin.py
@@ -35,13 +35,17 @@ def test_round_robin_invalid_players():
 
 
 def test_even_round_robin_everyone_plays_each_other():
+    # Arrange
     num_players = 10
     counts = defaultdict(set)
+
+    # Act
     for round in round_robin.round_robin(num_players):
         for p1, p2 in round:
             counts[p1].add(p2)
             counts[p2].add(p1)
 
+    # Assert
     assert num_players % 2 == 0
     assert len(counts) == num_players
     for p in counts:
@@ -50,13 +54,17 @@ def test_even_round_robin_everyone_plays_each_other():
 
 
 def test_odd_round_robin_everyone_plays_each_other():
+    # Arrange
     num_players = 11
     counts = defaultdict(set)
+
+    # Act
     for round in round_robin.round_robin(num_players):
         for p1, p2 in round:
             counts[p1].add(p2)
             counts[p2].add(p1)
 
+    # Assert
     assert num_players % 2 == 1
     assert len(counts) == num_players + 1  # new "dummy" player introduced
     dummy_player = len(counts)
@@ -71,34 +79,38 @@ def test_odd_round_robin_everyone_plays_each_other():
 
 
 def test_get_matchups_returns_seeded_players_by_rating():
-    players = [
-        Player(name='Picolo', rating=600),
-        Player(name='Trunks', rating=700),
-        Player(name='Vegeta', rating=900),
-        Player(name='Goten', rating=500),
-        Player(name='Goku', rating=1000),
-        Player(name='Gohan', rating=800),
-    ]
+    # Arrange
     round = [(1, 2), (3, 4), (5, 6)]
+    players = [Player(name='Picolo', rating=600),
+               Player(name='Trunks', rating=700),
+               Player(name='Vegeta', rating=900),
+               Player(name='Goten', rating=500),
+               Player(name='Goku', rating=1000),
+               Player(name='Gohan', rating=800)]
 
-    matchups = round_robin.get_matchups(round, players)
+    # Act
+    result = round_robin.get_matchups(round, players)
 
-    assert len(matchups) == 3
+    # Assert
+    assert len(result) == 3
 
-    matchup1 = matchups[0]  # 1 vs 2
-    assert matchup1[0][1] == 1
-    assert matchup1[1][1] == 2
-    assert matchup1[0][0].name == 'Goku'
-    assert matchup1[1][0].name == 'Vegeta'
+    (p1, p1_seed), (p2, p2_seed) = result[0]  # 1 vs 2
+    assert p1.name == 'Goku'
+    assert p2.name == 'Vegeta'
+    assert p1_seed == 1
+    assert p2_seed == 2
+    assert p1.rating >= p2.rating
 
-    matchup2 = matchups[1]  # 3 vs 4
-    assert matchup2[0][1] == 3
-    assert matchup2[1][1] == 4
-    assert matchup2[0][0].name == 'Gohan'
-    assert matchup2[1][0].name == 'Trunks'
+    (p1, p1_seed), (p2, p2_seed) = result[1]  # 3 vs 4
+    assert p1.name == 'Gohan'
+    assert p2.name == 'Trunks'
+    assert p1_seed == 3
+    assert p2_seed == 4
+    assert p1.rating >= p2.rating
 
-    matchup3 = matchups[2]  # 5 vs 6
-    assert matchup3[0][1] == 5
-    assert matchup3[1][1] == 6
-    assert matchup3[0][0].name == 'Picolo'
-    assert matchup3[1][0].name == 'Goten'
+    (p1, p1_seed), (p2, p2_seed) = result[2]  # 5 vs 6
+    assert p1.name == 'Picolo'
+    assert p2.name == 'Goten'
+    assert p1_seed == 5
+    assert p2_seed == 6
+    assert p1.rating >= p2.rating
