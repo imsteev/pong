ceb6cdbf958eb20be484f3bc5ad702a98bdc4652
Stephen Chung <spchung95@gmail.com>
2019-11-17 22:18:13 -0800

    re-use connection

--- db.py
+++ db.py
@@ -3,14 +3,11 @@ import sqlite3
 
 DB_PATH = '/tmp/pong.db'
 
-# conn.execute requires an Iterable to be passed to params. This is for convenience.
-NO_DATA = []
-
 
 def create_db():
     should_create = False
     if os.path.exists(DB_PATH):
-        confirm = input(f'{DB_PATH} already exists. Do you want to recreate it? (Y/y/Yes/yes) ')
+        confirm = input(f'{DB_PATH} already exists. Do you want to recreate it? (Y/y/N/n) ')
         try:
             confirm = confirm.lower()
         except Exception:
@@ -25,37 +22,41 @@ def create_db():
     else:
         should_create = True
 
-    if should_create:
-        conn = sqlite3.connect(DB_PATH)
-        conn.execute(
-            """
-            CREATE TABLE player (
-                id INTEGER PRIMARY KEY,
-                name TEXT NOT NULL,
-                rating INTEGER
-            )
-            """
+    if not should_create:
+        return
+
+    conn = sqlite3.connect(DB_PATH)
+    conn.execute(
+        """
+        CREATE TABLE player (
+            id INTEGER PRIMARY KEY,
+            name TEXT NOT NULL,
+            rating INTEGER
         )
-        conn.close()
-        print(f'Successfully created db at {DB_PATH}')
+        """
+    )
+    conn.close()
+    print(f'Successfully created db at {DB_PATH}')
 
 
 def insert_players(vals):
     conn = sqlite3.connect(DB_PATH)
     conn.executemany("INSERT INTO player (name, rating) VALUES (?, ?)", vals)
-    conn.commit()
+    conn.close()
 
 
 def select_players(*ids):
     conn = sqlite3.connect(DB_PATH)
-    data = NO_DATA
-    query = "SELECT * FROM player"
     if ids:
         data = ','.join([str(i) for i in ids])
-        query += " WHERE id IN (?)"
-    res = conn.execute(query, data)
-    print(list(res))
+        query = "SELECT * FROM player WHERE id IN (?)"
+        res = conn.execute(query, data)
+    else:
+        res = conn.execute("SELECT * FROM player")
+    players = list(res)
+    conn.close()
+    return players
 
 
 if __name__ == "__main__":
-    create_db()
+    pass
